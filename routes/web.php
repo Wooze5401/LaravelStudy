ce867a1458543db45b1c2c954d9394aacd8a182e
vagrant <vagrant@homestead>
2018-07-11 04:14:32 +0000

    controller static pages

--- routes/web.php
+++ routes/web.php
@@ -15,18 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello/{id?}',function ($id=1) {
-   return 'hello'.$id;
-})->name('test');
-Route::get('redirect',function (){
-    return redirect(route('test'));
-});
-
-Route::get('form',function (){
-    return '<form method="POST" action="/foo">
-        '.csrf_field().'
-        <button type="submit">提交</button></form>';
-});
-Route::match(['get','post'],'foo',function (){
-    return 'good request!';
-});
+Route::get('/', 'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
